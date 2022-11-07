ef376d122c4d0a0a9fa1ea4a50e9db7eff5fc068
Samet <sametatass@gmail.com>
2022-11-07 18:02:49 +0300

    Add project files.

---
+++ TukcellKursWorking/Controllers/ExampleController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TukcellKursWorking.Models;
+
+namespace TukcellKursWorking.Controllers
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+        
+    public class ExampleController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.sayi = 1;
+            ViewData["names"] = new List<String>() {"samet", "mehmet", "emre"};
+            var products = new List<Product>()
+            {
+                new(){Id = 1, Name = "Samet" },
+                new(){Id = 2, Name = "Berna" }
+            };
+            return View(products);
+        }
+        // Action Results
+        public IActionResult Index2()
+        {
+            TempData["age"] = 32;
+            return View();
+        }
+        public IActionResult ContentResult() 
+        {
+            return Content("denemem");
+        }
+        public IActionResult JSonResult()
+        {
+            return Json(new {id = 1, name = "samet" , age= 21 });
+        }
+        public IActionResult EmptyResult()
+        {
+            return EmptyResult();
+        }
+        public IActionResult RedirectToActionResult()
+        {
+            return RedirectToAction("Index","Example");
+        }
+    }
+}
---
+++ TukcellKursWorking/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TukcellKursWorking.Models;
+
+namespace TukcellKursWorking.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)// her controllerın view'de karşılığı vardır (Klasör)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()// view'de karşılığı var ise action Methoddur.
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
